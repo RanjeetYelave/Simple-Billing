2480a6c703f58a24e49a4ff42438a725bf814262
Ranjeet Yelave <126166466+RanjeetYelave@users.noreply.github.com>
2025-11-21 15:45:58 +0530

    Pilot

---
+++ billsoft/src/main/java/com/billing/simple/billsoft/BillsoftApplication.java
@@ -0,0 +1,13 @@
+package com.billing.simple.billsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsoftApplication.class, args);
+	}
+
+}
---
+++ billsoft/src/test/java/com/billing/simple/billsoft/BillsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.simple.billsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
